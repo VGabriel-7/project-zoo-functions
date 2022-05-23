82b0887f019c8e6c2f0606a39602ad72dd3c447a
Vinicius Gabriel <viniciusgsa99@gmail.com>
2022-05-23 14:10:13 -0300

    Requisito 4 concluído, implementando a função getEmployeeByName.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (employeeName) => {
+  const [objectEmployee = {}] = data.employees
+    .filter(({ firstName, lastName }) => employeeName === firstName || employeeName === lastName);
+  return objectEmployee;
+};
 
 module.exports = getEmployeeByName;
