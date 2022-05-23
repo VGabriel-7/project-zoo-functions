b68a45429435f46de9b6527cf2372cac6de23c4f
Vinicius Gabriel <viniciusgsa99@gmail.com>
2022-05-23 17:51:06 -0300

    Requisito 5 conluído, emplementando função countAnimals.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = (animal) => {
+  if (animal === undefined) {
+    return data.species.reduce((acc, { name, residents }) => ({
+      ...acc,
+      [name]: residents.length,
+    }), {});
+  }
+  const filteredAnimal = data.species.find(({ name }) => animal.specie === name).residents;
+  const arrayAnimal = Object.keys(animal);
+  if (arrayAnimal.includes('sex')) {
+    return filteredAnimal.reduce((acc, { sex }) => ((sex === animal.sex) ? acc + 1 : acc + 0), 0);
+  }
+  return filteredAnimal.length;
+};
 
 module.exports = countAnimals;
