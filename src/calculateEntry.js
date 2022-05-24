b0efff9f56a602d3513dab1db4f8303099706da6
Vinicius Gabriel <viniciusgsa99@gmail.com>
2022-05-24 14:49:28 -0300

    Requisito 8 concluído, implementando a função calculateEntry e countEntrants.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  const countsAges = { adult: 0, senior: 0, child: 0 };
+  entrants.forEach(({ age }) => {
+    if (age < 18) {
+      countsAges.child += 1;
+    } else if (age >= 18 && age < 50) {
+      countsAges.adult += 1;
+    } else {
+      countsAges.senior += 1;
+    }
+  });
+  return countsAges;
+};
 
-function calculateEntry(entrants) {
-  // código aqui
-}
+const calculateEntry = (entrants) => {
+  if (entrants === undefined || Object.keys(entrants).length === 0) { return 0; }
+  const allPrices = Object.values(prices);
+  return Object.values(countEntrants(entrants))
+    .reduce((acc, curr, idx) => (curr * allPrices[idx]) + acc, 0);
+};
 
 module.exports = { calculateEntry, countEntrants };
